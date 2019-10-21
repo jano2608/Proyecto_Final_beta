71dee629ec763d5431bde21cc646010f0d21a142
jano2608 <boris.mora.barra@gmail.com>
2019-10-21 17:08:33 -0300

    add name devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'users', to: 'users#index', as: 'user'
-
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   resources :posts
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
